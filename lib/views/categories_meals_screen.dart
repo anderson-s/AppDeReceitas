5c827643c30be9b00018449c856b76cf288eda73
Andeson Silva <andsilva607@gmail.com>
2023-01-18 14:07:40 -0300

    Adicionando novo componente para refeições

--- lib/views/categories_meals_screen.dart
+++ lib/views/categories_meals_screen.dart
@@ -1,3 +1,5 @@
+import 'package:app_receitas/models/dummy_data.dart';
+import 'package:app_receitas/views/components/meal_item.dart';
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
@@ -9,13 +11,18 @@ class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
+    final categoryMeals = dummyMeals
+        .where((element) => element.categories.contains(category.id))
+        .toList();
     return Scaffold(
-      appBar: AppBar(
-        title: Text(category.title),
-      ),
-      body: const Center(
-        child: Text("Receitas"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(category.title),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return MealItem(meal: categoryMeals[index]);
+          },
+          itemCount: categoryMeals.length,
+        ));
   }
 }
---
+++ lib/views/components/meal_item.dart
@@ -0,0 +1,39 @@
+import 'package:app_receitas/models/meal.dart';
+import 'package:flutter/material.dart';
+
+class MealItem extends StatelessWidget {
+  final Meal meal;
+  const MealItem({super.key, required this.meal});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          elevation: 4,
+          margin: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                    child: Image.network(
+                      meal.imageUrl,
+                      height: 200,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                ],
+              )
+            ],
+          )),
+    );
+  }
+}
