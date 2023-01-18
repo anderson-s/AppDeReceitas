81787c25e0e92380a6c8bdef7a75f6ae0f91ba72
Andeson Silva <andsilva607@gmail.com>
2023-01-18 18:31:13 -0300

    Adicionando BottomNavigatorBar

--- lib/views/tabs.dart
+++ lib/views/tabs.dart
@@ -2,8 +2,25 @@ import 'package:app_receitas/views/categories_screen.dart';
 import 'package:app_receitas/views/meals_favorite.dart';
 import 'package:flutter/material.dart';
 
-class Tabs extends StatelessWidget {
+class Tabs extends StatefulWidget {
   const Tabs({super.key});
+
+  @override
+  State<Tabs> createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  int index = 0;
+  final pages = [
+    {"title": "Lista de Categorias", "page": const CategoriesScreen()},
+    {"title": "Favoritos", "page": const MealsFavorite()},
+  ];
+
+  _selecionarPagina(int valor) {
+    setState(() {
+      index = valor;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,23 +28,32 @@ class Tabs extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text("Vamos Cozinhar?"),
+          title: Text(pages[index]["title"] as String),
           centerTitle: true,
-          bottom: const TabBar(tabs: [
-            Tab(
-              icon: Icon(Icons.category),
-              text: "Categorias",
-            ),
-            Tab(
-              icon: Icon(Icons.star),
-              text: "Favoritos",
-            ),
-          ]),
         ),
-        body: const TabBarView(
-          children: [
-            CategoriesScreen(),
-            MealsFavorite(),
+        body: pages[index]["page"] as Widget,
+        bottomNavigationBar: BottomNavigationBar(
+          // backgroundColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).colorScheme.secondary,
+          type: BottomNavigationBarType.shifting,
+          currentIndex: index,
+          onTap: _selecionarPagina,
+          items: [
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              icon: const Icon(
+                Icons.category,
+              ),
+              label: "Categorias",
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              icon: const Icon(
+                Icons.start,
+              ),
+              label: "Favoritos",
+            )
           ],
         ),
       ),
