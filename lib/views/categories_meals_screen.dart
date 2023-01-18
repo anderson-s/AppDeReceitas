212a2ebc37781d6d37a0df6a8743506747e80425
Andeson Silva <andsilva607@gmail.com>
2023-01-17 22:59:17 -0300

    Adicionando novas telas

---
+++ lib/views/categories_meals_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+  const CategoriesMealsScreen({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category.title),
+      ),
+      body: const Center(
+        child: Text("Receitas"),
+      ),
+    );
+  }
+}
--- lib/views/components/category_item.dart
+++ lib/views/components/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:app_receitas/models/category.dart';
+import 'package:app_receitas/views/categories_meals_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
@@ -7,22 +8,36 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).colorScheme.primary,
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => CategoriesMealsScreen(
+              category: category,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.headline6,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
       ),
     );
   }
